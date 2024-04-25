182b9ce4c7829634aa5278e29e708cbb5ef17d68
wcesaret <wcesaret@iu.edu>
2024-04-25 13:06:53 -0400

    done

--- ciphers_project/ciphers_project/urls.py
+++ ciphers_project/ciphers_project/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('ciphers/', include('api.urls'))
+    path('ciphers/', include('api.urls')),
+    path('', include('api.urls'))
 ]
